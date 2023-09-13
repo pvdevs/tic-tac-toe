d199543d0b93da2b85aaa80f06558b2e038451f8
Paulo Victor <paulovictor.devs@gmail.com>
2023-09-13 19:24:36 -0300

    add initial script

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log('Hello');
+
+const gambeBoard = [
+                    ['','',''],
+                    ['','',''],
+                    ['','','']
+                   ];
+
+
+
+const Player = (name, positionX, positionY) => {
+    const getName = () => name;
+
+
+}
+
+
+
+console.log(gambeBoard);
