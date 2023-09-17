d2b6fc2e2a4eee5c6e606b6693993ebc457ac3dc
Paulo Victor <paulovictor.devs@gmail.com>
2023-09-17 17:23:54 -0300

    code refactoring pt4

--- script.js
+++ script.js
@@ -1,19 +1,137 @@
 console.log('Hello');
+
 const board = document.querySelectorAll('.board-field');
 const displayTurn = document.querySelector('.display');
 const reset = document.getElementById('reset-btn');
-//Functions
 
+//Game state
 let gameIsOver = false;
 
+//Event Listeners
 board.forEach(field => field.addEventListener('click', gameEvent));
 reset.addEventListener('click', resetGame);
+
+// Objects
+
+const gameBoard = (() => {
+    const board = [];
+    const winningCombinations =[
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+    let playerBallTurn = false;
+    const isBallTurn = () => playerBallTurn; //Function used to export which round the game is currently;
+    const switchPlayer = () => playerBallTurn = !playerBallTurn;
+
+    const boardFill = position => board.push(position);
+    const boardIsFull = () => board.length === 9;
+
+    const positionFilled = position => board.includes(position);
+
+    let winningNumbers;
+    const getWinningNumbers = () => winningNumbers;
+
+    const winner = () => {
+        winningCombinations.forEach((combination) => {
+            const playerXWon = combination.every(field => playerX.playerFields.includes(field));
+            const playerBallWon = combination.every(field => playerO.playerFields.includes(field));
+
+            if(playerXWon || playerBallWon) winningNumbers = combination;
+            if(playerXWon) winner = 'playerX';
+            if(playerBallWon)winner = 'playerO';
+        });
+    }
+
+    const checkWin = () => {
+        let winner;
+
+        winningCombinations.forEach((combination) => {
+            const playerXWon = combination.every(field => playerX.playerFields.includes(field));
+            const playerBallWon = combination.every(field => playerO.playerFields.includes(field));
+
+            if(playerXWon || playerBallWon) winningNumbers = combination;
+            if(playerXWon) winner = 'playerX';
+            if(playerBallWon)winner = 'playerO';
+        });
+
+        if(winner != null) return winner;
+        else return null;
+    }
+
+    const reset = () => {
+        board.length = 0;
+        playerBallTurn = false;
+        winningNumbers = undefined;
+    }
+
+    return {board, checkWin, playerBallTurn, switchPlayer, isBallTurn, reset, winner, boardFill, positionFilled, boardIsFull, winningNumbers, getWinningNumbers}
+})();
+
+
+const displayController = (() => {
+    const tie = () => displayTurn.textContent = `Tie!`
+
+    const displayWinnerLine = () => {
+        const winningNumbers = gameBoard.getWinningNumbers();
+
+        winningNumbers.forEach((field) => {
+            const fieldToPaint = document.getElementById(`${field}`);
+            fieldToPaint.style.backgroundColor = '#AAFF00';
+        })
+    }
+
+    const displayWinner = winner => {
+        displayWinnerLine();
+        displayTurn.textContent = `${winner} wins!`
+    } 
+
+    const displayCurrentPlayer = () => gameBoard.isBallTurn() ? displayTurn.textContent = 'O is playing!' : displayTurn.textContent = 'X is playing!';
+
+    const displayMove = (position) => {
+        const field = document.getElementById(`${position}`);
+        if(gameBoard.isBallTurn() === false) field.classList.add('paint-x');
+        if(gameBoard.isBallTurn() === true) field.classList.add('paint-ball');
+    }
+
+    const reset = () => {
+        board.forEach((field) => {
+            field.classList.remove('paint-x');
+            field.classList.remove('paint-ball');
+            field.style.backgroundColor = '#FFFFFF';
+            displayTurn.textContent = 'X is playing!';
+        });
+    };
+    return {displayCurrentPlayer, displayMove, displayWinnerLine, displayWinner, reset, tie};
+})();
+
+//
+
+const Player = () => {
+    const playerFields = [];
+    const getMove = position => playerFields.push(position);
+
+    const reset = () => playerFields.length = 0;
+    return {playerFields, getMove, reset}
+}
+
+const playerX = Player();
+const playerO = Player();
+
+//
 
 function resetGame(){
     playerX.reset();
     playerO.reset();
     gameBoard.reset();
     displayController.reset();
+    gameIsOver = false;
 }
 
 function playRound(field) {
@@ -41,118 +159,3 @@ function gameEvent(event) {
       gameIsOver = true;
     }
 }
-
-// Objects
-
-
-const gameBoard = (() => {
-    const board = [];
-    const winningCombinations =[
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ]
-
-    let playerBallTurn = false;
-    const isBallTurn = () => playerBallTurn; //Function used to export which round the game is currently;
-    const switchPlayer = () => playerBallTurn = !playerBallTurn;
-
-    const boardFill = position => board.push(position);
-    const boardIsFull = () => board.length === 9;
-
-    const positionFilled = position => board.includes(position);
-
-
-    let winningNumbers;
-    const exportCombination = () => winningNumbers;
-
-    const winner = () => {
-        winningCombinations.forEach((combination) => {
-            const playerXWon = combination.every(field => playerX.playerFields.includes(field));
-            const playerBallWon = combination.every(field => playerO.playerFields.includes(field));
-
-            if(playerXWon || playerBallWon) winningNumbers = combination;
-            if(playerXWon) winner = 'playerX';
-            if(playerBallWon)winner = 'playerO';
-        });
-
-    }
-
-    const checkWin = () => {
-        let winner;
-
-        winningCombinations.forEach((combination) => {
-            const playerXWon = combination.every(field => playerX.playerFields.includes(field));
-            const playerBallWon = combination.every(field => playerO.playerFields.includes(field));
-            
-            if(playerXWon || playerBallWon) winningNumbers = combination;
-            if(playerXWon) winner = 'playerX';
-            if(playerBallWon)winner = 'playerO';
-        });
-        if(winner != null) return winner;
-        else return null;
-    }
-
-    const reset = () => playerBallTurn = false; // need to clear the board too.
-
-    return {board, checkWin, playerBallTurn, switchPlayer, isBallTurn, reset, winner, boardFill, positionFilled, boardIsFull, winningNumbers, exportCombination}
-})();
-
-////////////////////////////////////////////////////////////////////////
-
-const displayController = (() => {
-
-    const tie = () => displayTurn.textContent = `Tie!`
-
-    const displayWinner = winner => {
-        const winningNumbers = gameBoard.exportCombination();
-        winningNumbers.forEach((field) => {
-            const tempField = document.getElementById(`${field}`);
-            tempField.style.backgroundColor = '#AAFF00';
-        })
-        displayTurn.textContent = `${winner} wins!`
-
-    } 
-
-    const displayCurrentPlayer = () => gameBoard.isBallTurn() ? displayTurn.textContent = 'O is playing!' : displayTurn.textContent = 'X is playing!';
-
-    const displayMove = (position) => {
-        const field = document.getElementById(`${position}`);
-        if(gameBoard.isBallTurn() === false) field.classList.add('paint-x');
-        if(gameBoard.isBallTurn() === true) field.classList.add('paint-ball');
-    }
-    const reset = () => {
-        board.forEach((field) => {
-            field.classList.remove('paint-x')
-            field.classList.remove('paint-ball')
-            displayTurn.textContent = 'X is playing!'
-        });
-    };
-    return {displayCurrentPlayer, displayMove, displayWinner, reset, tie};
-})();
-
-////////////////////////////////////////////////////////////////////////
-
-const Player = (marker) => {
-    const playerFields = [];
-    const getPlayerMarker = () => marker;
-    const getMove = position => playerFields.push(position);
-
-    const reset = () => playerFields.length = 0;
-
-    return {getPlayerMarker, playerFields, getMove, reset}
-}
-
-const players = [];
-const playerX = Player('x');
-const playerO = Player('o');
-
-players.push(playerX);
-players.push(playerO);
-
-console.log(players);
